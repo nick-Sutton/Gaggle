225680922a419ee14d7b895c3c32d7b5fe35e53d
nick-Sutton <136986806+nick-Sutton@users.noreply.github.com>
2025-05-06 15:06:17 -0400

    Added player id

--- backend/internal/player/player.go
+++ backend/internal/player/player.go
@@ -1,16 +1,26 @@
 package player
 
-import "github.com/nick-Sutton/Gaggle/backend/internal/time"
+import (
+	"github.com/google/uuid"
+	"github.com/nick-Sutton/Gaggle/backend/internal/time"
+)
 
 type Player struct {
 	FirstName          string
 	LastName           string
 	Email              string
+	Id                 string
 	AvailableTimeSlots map[int]time.DailyTimeSlot // day -> availability
 	PreferedTimeSlots  []time.DailyTimeSlot
 }
 
-func NewPlayer(name string) *Player {
-	return &Player{FirstName: name,
-		AvailableTimeSlots: make(map[int]time.DailyTimeSlot)}
+func NewPlayer(firstName string, lastName string, email string) *Player {
+	p := &Player{}
+	p.FirstName = firstName
+	p.LastName = lastName
+	p.Email = email
+	p.Id = uuid.NewString()
+	p.AvailableTimeSlots = make(map[int]time.DailyTimeSlot)
+
+	return p
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nick-Sutton/Gaggle
 
 go 1.24.2
+
+require github.com/google/uuid v1.6.0
