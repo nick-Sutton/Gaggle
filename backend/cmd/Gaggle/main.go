eb15d145233cf19eabdbbd39484954767efcc475
Nora Cam <nncam@ncsu.edu>
2025-05-14 21:07:14 -0400

    Can open a CSV file at command line
    Console will take a CSV input file and report if it was opened successfully

--- backend/cmd/Gaggle/main.go
+++ backend/cmd/Gaggle/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/nick-Sutton/Gaggle/backend/internal/logo"
 )
 
 func main() {
 	logo.PrintConsoleLogo()
+
+	var path string
+	fmt.Println("Please enter the path to your CSV file:")
+	fmt.Scanln(&path)
+
+	file, err := os.Open(path)
+    if err != nil {
+		fmt.Println("Error opening file:", err)
+        os.Exit(1)
+    }
+
+	fmt.Println("Your file was succcessfully opened.")
+	defer file.Close()
 }
